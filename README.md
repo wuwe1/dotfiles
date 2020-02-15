401b12fb6bb50ab0a77e102e6f7c101f8e11a54c
wuwe1 <wu_we1@outlook.com>
2020-02-15 18:26:56 +0800

    modify README & alias

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # Usage
+
 1. clone the repo
 2. chmod +x ./install
 3. ./install
+
+# Making Local Customizations
+
+You can make local customizations for some programs by editing these files:
+
+* `vim` : `~/.vimrc_local`
+* `zsh` / `bash` : `~/.shell_local_before` run first
+* `zsh` : `~/.zshrc_local_before` run before `.zshrc`
+* `zsh` : `~/.zshrc_local_after` run after `.zshrc`
+* `zsh` / `bash` : `~/.shell_local_after` run last
+* `git` : `~/.gitconfig_local`
+* `tmux` : `~/.tmux_local.conf`
+
--- shell/aliases.sh
+++ shell/aliases.sh
@@ -19,9 +19,34 @@ dfu() {
     )
 }
 
+# Go up [n] directories
+up()
+{
+    local cdir="$(pwd)"
+    if [[ "${1}" == "" ]]; then
+        cdir="$(dirname "${cdir}")"
+    elif ! [[ "${1}" =~ ^[0-9]+$ ]]; then
+        echo "Error: argument must be a number"
+    elif ! [[ "${1}" -gt "0" ]]; then
+        echo "Error: argument must be positive"
+    else
+        for ((i=0; i<${1}; i++)); do
+            local ncdir="$(dirname "${cdir}")"
+            if [[ "${cdir}" == "${ncdir}" ]]; then
+                break
+            else
+                cdir="${ncdir}"
+            fi
+        done
+    fi
+    cd "${cdir}"
+}
+top10cmd(){
+history 1 | awk '{$1="";print substr($0,2)}' | sort | uniq -c | sort -n | tail -n 10
+}
+
 # Short cut
 alias zshconfig="vim ~/.zshrc"
 alias v="vim"
-alias top10cmd="history 1 | awk '{$1="";print substr($0,2)}' | sort | uniq -c | sort -n | tail -n 10"
 
 
